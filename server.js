4e293cdad2f91427a2530446269c8dc3a8492877
juativacoesms-ux <juativacoesms@gmail.com>
2026-02-25 14:44:08 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "meutoken123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Evento recebido:");
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando...");
+});
